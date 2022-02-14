5225db97f32521781c511dd5445d02f85330e6c2
Weston Perkins <westonperkins5@gmail.com>
2022-02-13 20:42:42 -0500

    refactoring for V6

---
+++ frontend/src/components/create-exercise.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CreateExercise extends Component {
+  render() {
+    return (
+      <div>Create exercise component</div>
+    )
+  }
+}
